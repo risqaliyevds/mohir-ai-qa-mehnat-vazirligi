fa5af6a55bf88b70504f2319d98f910db8480d74
mata <risqaliyevds@gmail.com>
2023-10-17 16:14:46 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from google.cloud import translate_v2
+import html
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from config import *
+from retriver import retriver
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from pprint import pprint
+
+
+def correct_unicode(text):
+    # Convert HTML entities to their respective characters
+    corrected_text = html.unescape(text)
+    return corrected_text
+
+
+def detect_and_translate(text, target_language='uz'):
+    # Initialize the Translate client
+    translate_client = translate_v2.Client()
+
+    # Detect the source language
+    detection = translate_client.detect_language(text)
+    source_language = detection['language']
+
+    # Translate the text to the target language (Russian)
+    translation = translate_client.translate(
+        text,
+        source_language=source_language,
+        target_language=target_language
+    )
+
+    # Extract and return the translated text
+    translated_text = translation['translatedText']
+    return translated_text
+
+
+def conversation(openai, retriver, CUSTOM_QUESTION_PROMPT):
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history", return_messages=True,)
+    chain = ConversationalRetrievalChain.from_llm(
+        openai,
+        retriever=retriver,  # see below for vectorstore definition
+        memory=memory,
+        condense_question_prompt=CUSTOM_QUESTION_PROMPT)
+
+    while True:
+        question = input("\nQanday savolingiz mavjud: ")
+        question = detect_and_translate(question, target_language='en')
+        answer = chain({"question": question})
+        answer = correct_unicode(
+            detect_and_translate(answer['answer'], target_language='uz'))
+        pprint(f"AI Assistant: {answer}")
+        ans = input(
+            "\n Yana savolingiz mavjudmi? (ha/yo'q) yoki (stop) yozing)")
+        if ans == 'stop' or ans == 'yo\'q':
+            break
+
+    pprint(memory.load_memory_variables({})['chat_history'])
+
+
+if __name__ == "__main__":
+    conversation(openai, retriver, CUSTOM_QUESTION_PROMPT)
---
+++ retriver.py
@@ -0,0 +1,41 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+
+class QASystem:
+    def __init__(self, doc_name) -> None:
+        self.doc_name = doc_name
+        self.db = None
+
+    def loader(self):
+        loader = PyPDFLoader(self.doc_name)
+        documents = loader.load()
+        return documents
+
+    def splitter(self):
+        documents = self.loader()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+        return texts
+
+    def createEmbeddings(self):
+        embeddings = OpenAIEmbeddings()
+        return embeddings
+
+    def checkDb(self):
+        if not self.db:
+            self.texts = self.splitter()
+            self.embeddings = self.createEmbeddings()
+            self.db = Chroma.from_documents(self.texts, self.embeddings)
+
+    def retriever(self):
+        self.checkDb()
+        retriever = self.db.as_retriever(
+            search_type="similarity", search_kwargs={"k": 2})
+        return retriever
+
+
+retriver = QASystem(
+    '/home/mata_/projects/qa for mehnat vazirligi/source/enFromRu.pdf').retriever()
