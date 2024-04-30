c8ee5aab25c59a7ec2521f34ab4095a0bf3d11ec
Muradjon Riskaliev <115413379+risqaliyevds@users.noreply.github.com>
2024-04-30 18:47:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from langchain import PromptTemplate
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "source/cogent-repeater-401107-83a21ee7d22b.json"
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 os.environ['BOOK_PATH'] = "source/Mehnat kodeksi en.pdf"
 os.environ['BOOK_NAME'] = "mohir-ai-demo-mehnat-vazirligi"
 os.environ['BOT_LOGO_PATH'] = "source/logo.jpg"
